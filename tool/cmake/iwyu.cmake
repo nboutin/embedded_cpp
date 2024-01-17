e1859ce6ea89cafc95b48bff2bac433f7309df12
Nicolas Boutin <boutwork@gmail.com>
2024-01-17 21:29:11 +0100

    Add IWYU sandbox project

--- tool/cmake/iwyu.cmake
+++ tool/cmake/iwyu.cmake
@@ -1,1 +1,8 @@
-set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE iwyu)
+# https://www.kitware.com//static-checks-with-cmake-cdash-iwyu-clang-tidy-lwyu-cpplint-and-cppcheck/
+# https://include-what-you-use.org/
+# https://stackoverflow.com/questions/30951492/how-can-i-use-the-tool-include-what-you-use-together-with-cmake-to-detect-unus
+
+set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE include-what-you-use;-Xiwyu;--verbose=3)
+set(CMAKE_C_INCLUDE_WHAT_YOU_USE include-what-you-use;-Xiwyu;--verbose=3)
+# set(CMAKE_CXX_INCLUDE_WHAT_YOU_USE include-what-you-use;-Xiwyu;--error=0;-Xiwyu;--verbose=3)
+# set(CMAKE_C_INCLUDE_WHAT_YOU_USE include-what-you-use;-Xiwyu;--error=0;-Xiwyu;--verbose=3)
